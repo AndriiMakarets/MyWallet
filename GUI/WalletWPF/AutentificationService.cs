b38604b3323a3279c5e0e63001359ab24a9dd7bf
Андрій Макарець <andrii.makarets@ukma.edu.ua>
2021-04-10 21:41:08 +0300

    Daria asked

---
+++ GUI/WalletWPF/AutentificationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletWPF
+{
+    public  class AutentificationService
+    {
+        public User Authenticate(AuthenticationUser User)
+        {
+            if (String.IsNullOrWhiteSpace(User.Login) || String.IsNullOrWhiteSpace(User.Password))
+                throw new ArgumentException("Empty field");
+        }
+
+        public AutentificationService(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
