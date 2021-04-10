d5efdf500f65b053a9efe5f248493ccd30d4b1e4
Андрій Макарець <andrii.makarets@ukma.edu.ua>
2021-04-10 23:41:18 +0300

    something

---
+++ GUI/WalletWPF/Authentication/SignInViewModel.cs
@@ -0,0 +1,40 @@
+﻿namespace WalletWPF.Authentication
+{
+    public class SignInViewModel
+    {
+
+        private AuthenticationUser _authUser = new AuthenticationUser();
+        public string Login
+        {
+            get
+            {
+                return _authUser.Login;
+            }
+            set
+            {
+                if (_authUser.Login != value)
+                {
+                    _authUser.Login = value;
+                    OnPropertyChanged();
+                    SignInCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return _authUser.Password;
+            }
+            set
+            {
+                if (_authUser.Password != value)
+                {
+                    _authUser.Password = value;
+                    OnPropertyChanged();
+                    SignInCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+    }
+}
