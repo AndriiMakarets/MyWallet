f9d0fee0a91fc9577050911e664fc6271b612789
Андрій Макарець <andrii.makarets@ukma.edu.ua>
2021-04-10 20:47:51 +0300

    User constructor

--- GUI/WalletWPF/User.cs
+++ GUI/WalletWPF/User.cs
@@ -10,11 +10,22 @@ namespace WalletWPF
 {
     public partial class User
     {
-       public Guid Guid { get; }
+        public User(Guid guid, string firstName, string lastName, string email, string login)
+        {
+            Guid = guid;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Login = login;
+        }
+
+        public Guid Guid { get; }
 
        public string FirstName { get; }
 
-       public string Email { get; }
+        public string LastName { get; }
+
+        public string Email { get; }
 
 
        public string Login { get; }
